7d77fc24e49a755f2bb2d74c660fbb8f9ed2ba96
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-22 23:01:16 +0000

    Tower buttons now are linked to the name of the tower required to be spawned

---
+++ Assets/scripts/towers/TowerButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TowerButton : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject towerObject;
+
+	public GameObject TowerObject {
+		get {
+			return towerObject;
+		}
+	}
+}
---
+++ Assets/scripts/towers/TowerManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TowerManager : Singleton<TowerManager> {
+
+	private TowerButton towerButtonPressed;
+
+	public void SelectedTower (TowerButton towerSelected) {
+		Debug.Log(towerSelected);
+		towerButtonPressed = towerSelected;
+	}
+}
