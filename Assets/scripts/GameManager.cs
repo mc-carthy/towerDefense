9772c783a3b507ebda5a19b9c8a09866dbf94cba
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-22 21:42:00 +0000

    Enemy instantiation

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	public GameObject spawnPoint;
+	public GameObject[] enemies;
+	public int maxEnemiesOnScreen;
+	public int totalEnemies;
+	public int enemiesPerSpawn;
+
+	private int enemiesOnScreen = 0;
+
+	private void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Start () {
+		SpawnEnemy();
+	}
+
+	private void SpawnEnemy () {
+		if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) {
+			for (int i = 0; i < enemiesPerSpawn; i++) {
+				if (enemiesOnScreen < maxEnemiesOnScreen) {
+					GameObject newEnemy = Instantiate(enemies[Random.Range(0, enemies.Length)]) as GameObject;
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnScreen++;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager;
+
+	private void Awake () {
+		MakeSingleton();
+	}
+	
+	private void MakeSingleton () {
+		if (GameManager.instance == null) {
+			Instantiate(gameManager);
+		}
+	}
+}
