e381f3e1e80104e99e35412dfc0fa6e14a2fc78f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-23 18:43:43 +0000

    Towers now placeable

--- Assets/scripts/towers/TowerManager.cs
+++ Assets/scripts/towers/TowerManager.cs
@@ -1,11 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Singleton<TowerManager> {
 
 	private TowerButton towerButtonPressed;
 
+	private void Update () {
+		if (Input.GetMouseButton(0)) {
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+			PlaceTower(hit);
+		}
+	}
+
 	public void SelectedTower (TowerButton towerSelected) {
-		Debug.Log(towerSelected);
 		towerButtonPressed = towerSelected;
+	}
+
+	public void PlaceTower (RaycastHit2D hit) {
+		if (!EventSystem.current.IsPointerOverGameObject() && towerButtonPressed != null) {
+			GameObject newTower = Instantiate(towerButtonPressed.TowerObject);
+			newTower.transform.position = hit.transform.position;
+		}
 	}
 }
