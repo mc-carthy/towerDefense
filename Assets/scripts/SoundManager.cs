6ff9345c0fbc40b1c022acbc1249dc2ef8e36692
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-24 19:36:12 +0000

    Add SoundManager and import bgm and sfx

---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager> {
+
+	[SerializeField]
+	private AudioClip arrow;	
+	[SerializeField]
+	private AudioClip death;
+	[SerializeField]
+	private AudioClip fireball;
+	[SerializeField]
+	private AudioClip gameOver;
+	[SerializeField]
+	private AudioClip hit;
+	[SerializeField]
+	private AudioClip level;
+	[SerializeField]
+	private AudioClip newGame;
+	[SerializeField]
+	private AudioClip rock;
+	[SerializeField]
+	private AudioClip towerBuilt;
+
+	private AudioClip Arrow {
+		get {
+			return arrow;
+		}
+	}
+
+	private AudioClip Death {
+		get {
+			return death;
+		}
+	}
+
+	private AudioClip Fireball {
+		get {
+			return fireball;
+		}
+	}
+
+	private AudioClip GameOver {
+		get {
+			return gameOver;
+		}
+	}
+
+	private AudioClip Hit {
+		get {
+			return hit;
+		}
+	}
+
+	private AudioClip Level {
+		get {
+			return level;
+		}
+	}
+
+	private AudioClip NewGame {
+		get {
+			return newGame;
+		}
+	}
+
+	private AudioClip Rock {
+		get {
+			return rock;
+		}
+	}
+
+	private AudioClip TowerBuilt {
+		get {
+			return towerBuilt;
+		}
+	}
+}
